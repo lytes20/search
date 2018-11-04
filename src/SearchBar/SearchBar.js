0bc3c14f9e3eefc3878f04c4672d35bf11b902bc
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-11-04 15:04:55 +0300

    initial commit

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const SearchBar = () => (
+  <div>
+    <h1>Perform your search here</h1>
+  </div>
+);
---
+++ src/SearchBar/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {SearchBar} from './SearchBar';
+
+class Search extends Component {
+    state = {  }
+    render() {
+        return ( <SearchBar />);
+    }
+}
+
+export default Search;
