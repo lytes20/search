5b3993c223c8128d2ebd8c97740478492f87ffa1
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-11-12 22:17:49 +0300

    Add search functionality

--- src/SearchBar/actions.js
+++ src/SearchBar/actions.js
@@ -1,6 +1,21 @@
-export const simpleAction = () => dispatch => {
-  dispatch({
-    type: 'SIMPLE_ACTION',
-    payload: 'result_of_simple_action',
+import makeAxiosCall from './services';
+
+export const searchAction = data => {
+  return({
+    type: 'SEARCH',
+    payload: data,
   });
+};
+
+export const performSearch = searchValue => {
+  return dispatch => {
+    console.log('I get called with', searchValue);
+    const response = makeAxiosCall(searchValue);
+    response.then((data) => {
+      console.log('data', data);
+      dispatch(searchAction(data));
+    }).catch(error => {
+      console.log(error);
+    });
+  };
 };
